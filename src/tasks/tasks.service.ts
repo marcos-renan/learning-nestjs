db12bbd3475f2c8f364fec3a4224ed8d93b96189
marcos-renan <devmarcos7@gmail.com>
2026-05-31 10:12:08 -0300

    feat: ✨ adicionando dados mockados para testes

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,22 +1,61 @@
 import { Injectable } from '@nestjs/common';
+import { Task } from './entities/task.entity';
 
 @Injectable()
 export class TasksService {
 
+  private tasks:Task[] = [
+    {
+      id:1,
+      name: "Comprar pão.",
+      description: "Ir na padaria comprar pães.",
+      completed:false
+    },
+    {
+      id:2,
+      name: "Ir na academia.",
+      description: "Treinar e perder kilos.",
+      completed:false
+    },
+    {
+      id:3,
+      name: "Zerar Quake.",
+      description: "Zerar Quake e suas DLC's.",
+      completed:false
+    },
+    {
+      id:4,
+      name: "Dormir cedo.",
+      description: "Dormir antes das 11hrs da noite.",
+      completed:false
+    }
+  ];
+
   listAllTasks(){
-    return [{
-      id:1, task: "Comprar pão"
-    }]
+    return this.tasks;
   }
 
   findOneTask(id:string){
-    return [{
-      id:1, task: `Tarefa com id:${id}`
-    }]
+    const task = this.tasks.find(task => task.id === Number(id));
+
+    if(!task){
+      return JSON.stringify("A tarefa não existe.");
+    }
+
+    return task;
   }
 
   createTask(body: any){
-    return body;
+    const newId = this.tasks.length + 1;
+
+    const newTask = {
+      id: newId,
+      ...body
+    }
+
+    this.tasks.push(newTask);
+
+    return newTask;
   }
 
   updateTask(id: string, body:any){
