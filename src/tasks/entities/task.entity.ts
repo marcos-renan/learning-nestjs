06c5760759462be7023b2c95a1b1d871b67cef6d
marcos-renan <devmarcos7@gmail.com>
2026-05-31 22:15:44 -0300

    feat: ✨ adiciona orm prisma, model e migration junto ao db SQLite

--- src/tasks/entities/task.entity.ts
+++ src/tasks/entities/task.entity.ts
@@ -1,6 +1,7 @@
 export class Task {
-  id:number;
-  name:string;
-  description:string;
-  completed:boolean;
+  id!:number;
+  name!:string;
+  description!:string;
+  completed!:boolean;
+  createdAt?: Date;
 }
