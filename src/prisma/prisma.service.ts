befdd1ff86e621d988b7c28e7e11fc18ce0518b8
marcos-renan <devmarcos7@gmail.com>
2026-06-03 16:41:20 -0300

    refactor: ♻️ atualizando crud com prisma orm

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from '@prisma/client/index';
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+
+  constructor(){
+    const databaseUrl = process.env.DATABASE_URL;
+    if (!databaseUrl) {
+      throw new Error('DATABASE_URL is not defined');
+    }
+
+    const adapter = new PrismaBetterSqlite3({ url: databaseUrl });
+    super({adapter})
+  }
+}
